8fe4bdedabe2eb9a22e345c14160bbcb302456bf
awais512 <raza.awais25@gmail.com>
2021-09-12 07:46:52 +0500

    Setting up the project

---
+++ pages/register.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Register = () => {
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
+          <h1 className=''>Register Page</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
