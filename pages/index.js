40fd5e1702be76746edd7b71d98369900a0fe8f3
awais512 <raza.awais25@gmail.com>
2021-09-12 13:14:01 +0500

    Adding Public Folder

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ const Home = () => {
     <div className='container'>
       <div className='row'>
         <div className='col'>
-          <h1 className=''>Home Page</h1>
+          <h1 className='display-1 text-center'>Home Page</h1>
         </div>
       </div>
     </div>
